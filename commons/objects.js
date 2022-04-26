3667f4c122461430e987cd836f8cfb1294a364ce
Danilo Procopio <dan_mazda@hotmail.com>
2022-04-26 15:22:37 -0300

    project architecture

---
+++ commons/objects.js
@@ -0,0 +1,11 @@
+export const carsList = [
+  {
+    id: 1,
+    cardId: '1A',
+    name: "Audi A3 sedan",
+    img: "https://www.autoo.com.br/fotos/2017/12/960_720/audi_a3-sedan_2018_1_29122017_8308_960_720.jpg",
+    toOneHundred: 6.69,
+    engineCapacity: 1.984,
+    hp: 220
+  }
+]
--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 import express from "express";
-
+import dotenv from "dotenv";
+import { Sequelize, Op, } from "sequelize";
+import { carsList } from "./commons/objects.js";
 const app = express();
+dotenv.config();
+
+
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
@@ -10,9 +15,22 @@ app.listen(3000, () => {
 });
 
 app.get("/", (req, res) => {
-  res.render("index");
+  res.render("index", { carsList });
 });
 app.get("/signup", (req, res) => {
   res.render("signup");
 });
+app.get("/register", (req, res) => {
+  res.render("register");
+});
 
+
+app.post("/card", (req, res) => {
+  res.render("register");
+});
+app.post("/login", (req, res) => {
+  res.render("register");
+});
+app.post("/signup", (req, res) => {
+  res.render("register");
+});
