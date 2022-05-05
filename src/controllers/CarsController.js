3f7275bd03be31c866b386a6e5842694596159be
Danmazda <dan_mazda@hotmail.com>
2022-05-05 14:03:59 -0300

    all routes and messgaes working

--- src/controllers/CarsController.js
+++ src/controllers/CarsController.js
@@ -3,7 +3,8 @@ import Car from "../models/Cars.Model.js";
 export const getAll = async (req, res) => {
   try {
     const cars = await Car.findAll({});
-    res.render("index", { cars });
+    let message;
+    res.render("index", { cars, message });
   } catch (error) {
     res.status(500).send({ err: error.message });
   }
@@ -26,12 +27,14 @@ export const getOne = async (req, res) => {
 export const deleteOne = async (req, res) => {
   try {
     const { id } = req.params;
-    const car = await Car.destroy({
+    await Car.destroy({
       where: {
         id,
       },
     });
-    res.send("Card Deleted Succesfully");
+    const cars = await Car.findAll({});
+    const message = `${id} deleted succesfully`;
+    res.render("index", { cars, message });
   } catch (error) {
     res.status(500).send({ err: error.message });
   }
@@ -50,7 +53,7 @@ export const updateOne = async (req, res) => {
       hp,
       description,
     } = req.body;
-    const car = await Car.update(
+    await Car.update(
       { name, img, toOneHundred, capacity, maxVel, cylinder, hp, description },
       {
         where: {
@@ -58,7 +61,9 @@ export const updateOne = async (req, res) => {
         },
       }
     );
-    res.send("Card updated Succesfully");
+    const cars = await Car.findAll({});
+    const message = `${name} updated succesfully`;
+    res.render("index", { cars, message });
   } catch (error) {
     res.status(500).send({ err: error.message });
   }
@@ -66,9 +71,11 @@ export const updateOne = async (req, res) => {
 
 export const postCard = async (req, res) => {
   try {
-    console.log(req.body);
-    const car = await Car.create({ ...req.body });
-    res.send("Card Created Succesfully");
+    const { name } = req.body;
+    await Car.create({ ...req.body });
+    const cars = await Car.findAll({});
+    const message = `${name} created succesfully`;
+    res.render("index", { cars, message });
   } catch (error) {
     res.status(500).send({ err: error.message });
   }
